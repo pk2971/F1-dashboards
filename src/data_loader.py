149829f0424ff0a033373e9bbda31f38ff41eedf
Praharshita Kaithepalli <89590898+pk2971@users.noreply.github.com>
2025-11-05 21:15:14 -0500

    tele test7

--- src/data_loader.py
+++ src/data_loader.py
@@ -3,7 +3,7 @@ import streamlit as st
 @st.cache_data(show_spinner=True)
 def load_session(year, event, session_type):
     session = fastf1.get_session(year, event, session_type)
-    session.load(telemetry=False, weather=True)
+    session.load(telemetry=True, weather=True)
     return session
 @st.cache_data(show_spinner=True)
 def load_telemetry_session(year , event , session_type):
